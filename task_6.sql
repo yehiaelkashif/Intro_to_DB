39da4d9fe1392798e012bf76a44425c91c78148a
yehiasalah <yehiasalah74@gmail.com>
2024-10-20 19:39:48 +0300

    asd

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,6 @@
 -- Insert multiple rows into the Customers table
+
+
 INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
