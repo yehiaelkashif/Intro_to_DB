5162f21e40520051c12a8f5bae7a46a4d20a8b2c
yehiasalah <yehiasalah74@gmail.com>
2024-10-20 18:43:12 +0300

    cahnge

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,9 @@
+-- Create the database
+CREATE DATABASE alx_book_store;
+
+-- Use the database
+USE alx_book_store;
+
 -- Create Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -17,25 +23,4 @@ CREATE TABLE Books (
 -- Create Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
-);
-
--- Create Orders table
-CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
-
--- Create Order_Details table
-CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
+    customer
